57a656e486fe9d718e94ef1006be28e8316a763d
kostya <fedurovk@gmail.com>
2022-01-29 16:28:42 +0300

    qwe

--- lib/perfect/perfect.dart
+++ lib/perfect/perfect.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class Perfect extends StatelessWidget {
+class Perfect extends StatefulWidget {
   const Perfect({Key? key}) : super(key: key);
 
   @override
+  State<Perfect> createState() => _PerfectState();
+}
+
+class _PerfectState extends State<Perfect> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/dark.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+    _controller.setLooping(true);
+    _controller.play();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Expanded(
+          child: VideoPlayer(_controller),
+        ),
+      ],
+    );
   }
 }
